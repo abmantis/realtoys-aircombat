3b316735c11ccd5e7ab492dc960d68c1c94266c0
abmantis <amfcalt@gmail.com>
2011-06-08 23:58:49 +0100

    Minor

--- RealToys_AirCombat/include/ModelsManager.h
+++ RealToys_AirCombat/include/ModelsManager.h
@@ -13,8 +13,8 @@ public:
 		PhysicsBodyDataStructure()
 		{
 			collisionType = CONVEX;
-			mass = 0.5f;
-			gravity = -2.8f;
+			mass = 1;
+			gravity = -9.8f;
 
 			//collisionType = TREE;
 			//mass = 0;
